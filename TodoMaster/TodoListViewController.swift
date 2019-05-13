3b49500fccbdb805393cf57234f3535436545c84
Andras Pal <andr4s.pal@gmail.com>
2019-05-13 22:56:29 +0100

    Add new item functionality complete.

--- TodoMaster/TodoListViewController.swift
+++ TodoMaster/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     
     override func viewDidLoad() {
@@ -50,7 +50,32 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    //MARK: - Add New Items
     
-    
-}
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new TodoMaster Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on our UIAlert
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
 
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    
+    }
+
+}
